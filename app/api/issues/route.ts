cce25b6b3eae635dc61d60a61b4521b8d72b57dc
Priyansh Yawalkar <78356829+PriyanshYawalkar@users.noreply.github.com>
2024-02-18 00:43:58 +0530

    impliment client side validation

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -1,12 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import { title } from "process";
-import { z } from "zod";
 import prisma from "@/prisma/client";
-
-const createIssueSchema = z.object({
-    title: z.string().min(1).max(255),
-    description: z.string().min(1)
-})
+import { createIssueSchema } from "../../validationSchemas";
 
 export async function POST(request: NextRequest) {
     const body = await request.json();
